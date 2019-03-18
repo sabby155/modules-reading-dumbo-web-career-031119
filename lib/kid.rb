c089395cb319c6ac35299a4afe85dbc182a22640
Sabrina Chowdhury <nebula155@gmail.com>
2019-03-18 01:25:08 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -4,7 +4,7 @@ require_relative './fancy_dance.rb'
 
 class Kid 
   extend FancyDance::ClassMethods
-  include FancyDance::Instancemethods
+  include FancyDance::InstanceMethods
   attr_accessor :name
  
   def initialize(name)
