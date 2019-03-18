11c93976adffb927d72af996396b506f1d69096f
Sabrina Chowdhury <nebula155@gmail.com>
2019-03-18 01:18:30 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -16,6 +16,8 @@ module FancyDance
       "Thank you, thank you. It was a pleasure to dance for you all."
     end
   end
-  
+   def metadata
+      "This class produces objects that love to dance."
+    end
   end  
 end   
